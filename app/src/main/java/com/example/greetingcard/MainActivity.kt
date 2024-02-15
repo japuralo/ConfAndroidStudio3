6d211eae13944cd99c0c5e33248496bf8f63b885
japuralo <japuralo@unirioja.es>
2024-02-15 18:44:28 +0100

    Paso 4 hecho.

--- app/src/main/java/com/example/greetingcard/MainActivity.kt
+++ app/src/main/java/com/example/greetingcard/MainActivity.kt
@@ -23,7 +23,7 @@ class MainActivity : ComponentActivity()
             GreetingCardTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    Greeting("Javier Alejandro")
                 }
             }
         }
@@ -45,6 +45,6 @@ fun GreetingPreview()
 {
     GreetingCardTheme()
     {
-        Greeting("Javier")
+        Greeting("Javier Alejandro")
     }
 }
